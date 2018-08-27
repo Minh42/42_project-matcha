9fbec8873bb3d69859c7b0127280ae37d987e21b
audreylamy <audrey.lamy92@gmail.com>
2018-08-27 14:22:15 +0200

    likeComponent

--- src/components/LikesComponent.js
+++ src/components/LikesComponent.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
 import getAge from 'get-age';
+import { withRouter } from 'react-router-dom';
 
 class LikesComponent extends Component {
+
+	constructor(props) {
+        super(props)
+
+        this.showProfile = this.showProfile.bind(this)
+    }
+
+	showProfile() {
+		const user_id = document.getElementById("imageLike").alt
+		console.log("id", user_id)
+        this.props.history.push('/profileOtherUser/' + user_id);
+	}
+	
 	render() {
 		const { likes } = this.props
 		console.log(likes)
@@ -12,7 +26,7 @@ class LikesComponent extends Component {
 				<div key={ users.user_id } className="columns">
 					<div className="column is-5">
 						<figure className="image is-96x96">
-							<img src={ src } className="imageLike" alt="userLikes"/>
+							<img src={ src } id="imageLike" className="imageLike" alt={ users.user_id } onClick={this.showProfile}/>
 						</figure>
 					</div>
 					<div className="column is-5 is-offset-1">
@@ -25,4 +39,4 @@ class LikesComponent extends Component {
 	}
 }
 
-export default LikesComponent
+export default withRouter(LikesComponent)
